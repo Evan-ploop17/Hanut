2b38940a4748eea339e3fb3826d86966ae7e6efe
Esteban Salas <esalas21@uan.edu.co>
2020-09-29 20:22:06 -0500

    AuthActivity.kt se corrige error

--- app/src/main/java/com/example/hanut/AuthActivity.kt
+++ app/src/main/java/com/example/hanut/AuthActivity.kt
@@ -1,11 +1,71 @@
 package com.example.hanut
 
+import android.content.Intent
 import android.os.Bundle
+import android.widget.Button
+import androidx.appcompat.app.AlertDialog
 import androidx.appcompat.app.AppCompatActivity
+import com.google.firebase.analytics.FirebaseAnalytics
+import com.google.firebase.auth.FirebaseAuth
+import kotlinx.android.synthetic.main.auth_activity.*
 
 class AuthActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        setContentView(R.layout.auth_activity)
+
+        val analytics = FirebaseAnalytics.getInstance(this)
+        val bundle = Bundle()
+        bundle.putString("Mensaje", "Integracin de firebase completa")
+        analytics.logEvent("InitScreen", bundle)
+
+        setUp()
     }
+
+     fun setUp(){
+        title = "Autenticacion"
+        signUpBtn.setOnClickListener{
+            if (emailText.text.isNotEmpty() && passwordText.text.isNotEmpty()){
+                FirebaseAuth.getInstance()
+                    .createUserWithEmailAndPassword(emailText.text.toString(),
+                    passwordText.text.toString()).addOnCompleteListener{
+                    if(it.isSuccessful){
+                        showHome(it.result?.user?.email ?: "")
+                    }else{
+                        showAlert()
+                    }
+                }
+            }
+        }
+        logInBtn.setOnClickListener{
+            if (emailText.text.isNotEmpty() && passwordText.text.isNotEmpty()){
+                FirebaseAuth.getInstance().signInWithEmailAndPassword(emailText.text.toString(),
+                    passwordText.text.toString()).addOnCompleteListener{
+                    if(it.isSuccessful){
+                        showHome(it.result?.user?.email ?: "")
+                    }else{
+                        showAlert()
+                    }
+                }
+            }
+        }
+    }
+
+     fun showAlert(){
+        val builder = AlertDialog.Builder(this)
+        builder.setTitle("Error")
+        builder.setMessage("Se ha producido un error de autenticacion del usuario")
+        builder.setPositiveButton("Aceptar", null)
+        val dialog = builder.create()
+        dialog.show()
+    }
+
+     fun showHome(email:String){
+        val homeIntent = Intent(this, MainActivity::class.java).apply {
+            putExtra("email", email)
+        }
+        startActivity(homeIntent)
+    }
+
+
 }

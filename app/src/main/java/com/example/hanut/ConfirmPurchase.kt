0b66454e9af2fda2d5e39ec1ec1fcfde5c14c41a
Esteban Salas <esalas21@uan.edu.co>
2020-11-02 01:41:59 -0500

    Corrections

--- app/src/main/java/com/example/hanut/ConfirmPurchase.kt
+++ app/src/main/java/com/example/hanut/ConfirmPurchase.kt
@@ -2,10 +2,17 @@ package com.example.hanut
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.WindowManager
+import androidx.core.content.ContextCompat
 
 class ConfirmPurchase : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_confirm_purchase)
+
+        val  window = this.window
+        window.addFlags(WindowManager.LayoutParams.FLAG_DRAWS_SYSTEM_BAR_BACKGROUNDS)
+        window.clearFlags(WindowManager.LayoutParams.FLAG_TRANSLUCENT_STATUS)
+        window.statusBarColor = ContextCompat.getColor(this, R.color.colorPrimaryDark)
     }
 }

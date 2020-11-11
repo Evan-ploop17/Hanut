45945b1d4f7b34167575dc660a4fe211b93c7455
Esteban Salas <esalas21@uan.edu.co>
2020-11-10 23:18:19 -0500

    Se le agrega algo de base de datos y se implementa la clase ActionBarHanut.kt para no escribir el mismo código en todos los activities

---
+++ app/src/main/java/com/example/hanut/ActionBarHanut.kt
@@ -0,0 +1,41 @@
+package com.example.hanut
+
+import android.app.Activity
+import android.content.Context
+import android.content.Intent
+import androidx.core.content.ContextCompat.startActivity
+import com.google.android.material.appbar.MaterialToolbar
+import kotlinx.android.synthetic.main.activity_main.*
+
+class ActionBarHanut {
+
+    fun callActionBar(actionBar: MaterialToolbar, context: Activity){
+
+        actionBar.setOnMenuItemClickListener { menuItem ->
+            when (menuItem.itemId) {
+                R.id.search -> {
+                    // Handle search icon press
+
+                    val intent = Intent(context, SearchActivity::class.java)
+                    context.startActivity(intent)
+                    //startActivity(intent)
+                    true
+                }
+
+                R.id.Porfile -> {
+                    // Handle search icon press
+
+                    val intent = Intent(context, Profile::class.java)
+                    context.startActivity(intent)
+                    //startActivity(intent)
+                    true
+                }
+
+                else -> false
+            }
+        }
+
+
+    }
+
+}
